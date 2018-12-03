7ef15e8d62840b1778942d0f9ab5405b96521a63
lxp <lxp@ufee.local>
2018-12-03 15:20:30 +0800

    Initial Commit

---
+++ BJ80/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BJ80
+//
+//  Created by lxp on 2018/12/3.
+//  Copyright © 2018年 lxp. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
